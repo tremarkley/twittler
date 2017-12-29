09d604a97a53cdcc53f26d97416dc07e9f9e9b98
Harry Markley <tremarkley@gmail.com>
2017-12-28 22:51:29 -0700

    Wrote methods to display a specified number of tweets (displayNTweets and printTweet) and hook it up to a timer in order to randomly generate tweets.

--- display_tweets.js
+++ display_tweets.js
@@ -1,14 +1,34 @@
 $(document).ready(function(){
     var $body = $('body');
     $body.html('');
+    var indexTracker = {'startingIndex': 0};
+      window.setInterval(function(){
+          displayNTweets(indexTracker, Math.floor(Math.random()*20 + 1));
+      }, (Math.random() * 10000) + 10)
+});
 
-    var index = streams.home.length - 1;
-    while(index >= 0){
-      var tweet = streams.home[index];
-      var $tweet = $('<div></div>');
-      $tweet.text('@' + tweet.user + ': ' + tweet.message);
-      $tweet.appendTo($body);
-      index -= 1;
-    }
+  function displayNTweets(indexObj, numberofTweetsToDisplay)
+  {
+      var startingIndex = indexObj.startingIndex;
+      console.log(startingIndex);
+      var tweetsAvailable = streams.home.length - startingIndex;
+      if (tweetsAvailable < numberofTweetsToDisplay)
+      {
+        numberofTweetsToDisplay = tweetsAvailable;
+      }
+      var endingIndex = startingIndex + numberofTweetsToDisplay;
+      for (let i = startingIndex; i < endingIndex; i++)
+      {
+        var tweet = streams.home[i];
+        printTweet(tweet);
+      }
+      indexObj.startingIndex = endingIndex;
+  }
 
-  });
+  function printTweet(tweet)
+  {
+    var $body = $('body');
+    var $tweet = $('<div></div>');
+    $tweet.text('@' + tweet.user + ': ' + tweet.message);
+    $tweet.appendTo($body);
+  }
