36070b816a8ee969dd476866f35c8423b6a8d12c
Harry Markley <tremarkley@gmail.com>
2017-12-29 21:51:37 -0700

    Add ability to display user tweets stored in localStorage on page

--- user_page.js
+++ user_page.js
@@ -4,8 +4,13 @@ $(document).ready(function() {
     if (localStorage.getItem('userTweets') !== undefined)
     {
         userTweets = JSON.parse(localStorage.getItem('userTweets'));
+        console.log(userTweets);
     }else {
         userTweets = streams.users;
     }
     var user = getURLParamValue('user');
+    userTweets = userTweets[user];
+    var indexObj = {'startingIndex' : 0};
+    var numTweetsAvailable = userTweets.length;
+    displayNTweets(indexObj, numTweetsAvailable, userTweets);
 });
