c466e7e309e0313d72b104247a13a10cb7853243
Harry Markley <tremarkley@gmail.com>
2017-12-29 21:51:01 -0700

    Change home_page.js to pass in a tweets object to displayNTweets

--- home_page.js
+++ home_page.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     var indexTracker = {'startingIndex': 0};
       window.setInterval(function display(){
-          displayNTweets(indexTracker, Math.floor(Math.random()*20 + 1)); 
+          displayNTweets(indexTracker, Math.floor(Math.random()*20 + 1), streams.home); 
           //will be used to get tweets for user page
           localStorage.setItem('userTweets', JSON.stringify(streams.users))
           return display;
